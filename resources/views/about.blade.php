921d85b67cae31ae8387f387229eafd63af2c554
Alex B <mabrave1106@gmail.com>
2020-06-15 19:39:17 -0400

    fixed ui added CSS fonts
    Signed-off-by: Alex B <mabrave1106@gmail.com>

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('layouts.app')
 @section('title', 'About Us')
 @section('content')
 <h1>About Us</h1>
--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('layouts.app')
 @section('title', 'Edit Details for '. $customer->name)
 @section('content')
     <div class="row">
